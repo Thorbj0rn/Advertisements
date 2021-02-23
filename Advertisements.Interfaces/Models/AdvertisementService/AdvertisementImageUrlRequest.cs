a40aabdbf082a007e492d175f373cda4dc3ab4ea
Bjorn <wwildwindd@gmail.com>
2021-02-23 16:09:50 +0200

    Добавлена валидация модели AdvertisementImageUrlRequest.

--- Advertisements.Interfaces/Models/AdvertisementService/AdvertisementImageUrlRequest.cs
+++ Advertisements.Interfaces/Models/AdvertisementService/AdvertisementImageUrlRequest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Advertisements.Interfaces.Models.AdvertisementService
@@ -12,14 +13,17 @@ namespace Advertisements.Interfaces.Models.AdvertisementService
         /// <summary>
         /// Идентификатор
         /// </summary>
+        [Required(ErrorMessage = "Введите идентификатор объявления.")]
         public Guid Id { get; set; }
         /// <summary>
         /// Ширина
         /// </summary>
+        [Required(ErrorMessage = "Введите ширину изображения.")]
         public int Width { get; set; }
         /// <summary>
         /// Высота
         /// </summary>
+        [Required(ErrorMessage = "Введите высоту изображения.")]
         public int Heigth { get; set; }
     }
 }
